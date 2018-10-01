3d727a301629316cc54834edf1c9ad54c6d616f1
NoHS <noh0355@likelion.org>
2018-10-01 16:07:06 +0000

    popup창 수정

--- app/controllers/gnu_controller.rb
+++ app/controllers/gnu_controller.rb
@@ -1,7 +1,9 @@
 class GnuController < ApplicationController
     def event
       @name =["TimeAndMap"]
-      @noon =[1,2,3,4,5,6]
-      @night=[7,8,9,10,11,12,13,14,15]
+      @noon =[1,2,3,4] 
+      @noon1 =[5,6]
+      @night=[7,8,9,10]
+      @night1 =[11,12,13,14]
     end
 end
